fdd459db3306d6bf885da958f786e34a3006f753
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-13 00:36:00 +0530

    added navbar

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Add = () => {
     const [cont, changeData] = useState(
@@ -34,6 +35,7 @@ const Add = () => {
     }
     return (
         <div>
+            <Navbar/>
             <div class="card text-center mb-3">
                 <div class="card-body">
                     <h5 class="card-title">Add Contacts</h5>
