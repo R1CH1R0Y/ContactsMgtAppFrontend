3ec8739df755c6bc77f94f358c3b96bc7443adc5
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-13 00:10:22 +0530

    succesfully read input and displayed corresponding output on table

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,54 +1,69 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Search = () => {
-    const [cont, changeData] = useState(
-        {
-            "fname": "",
-        }
-    )
-    const inputHandler=(event)=>{
-        changeData({...cont,[event.target.name]:event.target.value})
-    }
-    const readValue=()=>{
-        console.log(cont)
-    }
+  const [cont, changeData] = useState({
+    fname: '',
+  });
+  const [result, setData] = useState([]);
+
+  const inputHandler = (event) => {
+    changeData({ ...cont, [event.target.name]: event.target.value });
+  };
+
+  const readValue = () => {
+    console.log(cont);
+    axios.post("http://localhost:8335/search", cont).then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    });
+  };
+
   return (
     <div>
-        <div class="card text-center mb-3">
-  <div class="card-body">
-    <h5 class="card-title">Search-Delete Contacts</h5>
-    <p></p>
-    <p></p>
-    <div className="container">
-        <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+      <div class="card text-center mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Search-Delete Contacts</h5>
+          <p></p>
+          <p></p>
+          <div className="container">
+            <div className="row">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <label htmlFor="" className="form-label">First Name :</label>
-                <input type="text" className="form-control" name='fname' value={cont.fname} onChange={inputHandler}/>
+                <input type="text" className="form-control" name='fname' value={cont.fname} onChange={inputHandler} />
+              </div>
             </div>
+          </div>
+          <br />
+          <button className="btn btn-warning" onClick={readValue}>Search</button>
+          <br />
+          <br />
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Mobile .</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((value, index) => (
+                <tr key={index}>
+                  <td>{value.fname}</td>
+                  <td>{value.lname}</td>
+                  <td>{value.mob}</td>
+                  <td>{value.email}</td>
+                  <td>{value.address}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
-    <br />
-    <button className="btn btn-warning" onClick={readValue}>Search</button>
-    <br />
-    <br />
-    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">Mobile .</th>
-                                <th scope="col">E-mail</th>
-                                <th scope="col">Address</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                       
-                        </tbody>
-                    </table>
-  </div>
-</div>
-    </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
