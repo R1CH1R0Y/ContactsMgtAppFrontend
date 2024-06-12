03a4b485cd7c35c555a41be00b05558a1760e897
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-12 23:54:57 +0530

    structure of search-delete page completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Search = () => {
+
   return (
     <div>
         <div class="card text-center mb-3">
@@ -11,7 +12,7 @@ const Search = () => {
     <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <label htmlFor="" className="form-label">Name/Mobile :</label>
+                <label htmlFor="" className="form-label">First Name/Last Name/Mobile :</label>
                 <input type="text" className="form-control" />
             </div>
         </div>
@@ -31,7 +32,20 @@ const Search = () => {
                             </tr>
                         </thead>
                         <tbody>
+                        <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Mobile .</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             
+                        </tbody>
+                    </table>
                         </tbody>
                     </table>
   </div>
